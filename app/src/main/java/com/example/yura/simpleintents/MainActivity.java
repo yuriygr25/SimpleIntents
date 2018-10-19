3c310942fe54e562a46e1be17509387180ad1252
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-19 09:44:28 +0300

    commit

---
+++ app/src/main/java/com/example/yura/simpleintents/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.yura.simpleintents;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button btnWeb;
+    Button btnMap;
+    Button btnCall;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnWeb = (Button) findViewById(R.id.btnWeb);
+        btnMap = (Button) findViewById(R.id.btnMap);
+        btnCall = (Button) findViewById(R.id.btnCall);
+
+        btnWeb.setOnClickListener(this);
+        btnMap.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btnWeb:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
+                startActivity(intent);
+                break;
+            case R.id.btnMap:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:CFG7+HP"));
+                startActivity(intent);
+                break;
+            case R.id.btnCall:
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:12345"));
+                startActivity(intent);
+                break;
+        }
+    }
+}
